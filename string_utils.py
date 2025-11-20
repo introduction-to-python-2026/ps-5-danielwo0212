1b73f71b5bd813c67058f1b0cdfd1506cf25e47b
danielwo0212 <danielwo0212@gmail.com>
2025-11-20 20:58:07 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,3 +1,32 @@
+def split_before_each_uppercases(formula):
+  split_formula = []
+  start = 0
+  end = 1
+  if formula != "":
+    for i in range(1, len(formula)):
+      if formula[i].isupper():
+        end = i
+        split_formula.append(formula[start:end])
+        start = i
+    split_formula.append(formula[start:len(formula)])
+    return split_formula
+  else:
+    return []
+
+def split_at_first_digit(formula):
+  digit_location = 1
+  for i in range(1, len(formula)):
+    if formula[i].isdigit() == True:
+      break
+    else:
+      digit_location += 1
+  if digit_location == len(formula):
+    return formula, 1
+  else:
+    prefix = formula[:digit_location]
+    numeric = int(formula[digit_location::])
+    return prefix,numeric
+
 def count_atoms_in_molecule(molecular_formula):
     atom_count = {}
     for i in split_before_each_uppercases(molecular_formula):
